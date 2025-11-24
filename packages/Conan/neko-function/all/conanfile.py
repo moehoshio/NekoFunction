3fe48e5066e742b2e19518385b26198e4e4d9cae
moehoshio <132435834+moehoshio@users.noreply.github.com>
2025-11-25 05:22:54 +0900

    Update version to 1.0.8 and modify related configuration files

--- packages/Conan/neko-function/all/conanfile.py
+++ packages/Conan/neko-function/all/conanfile.py
@@ -6,7 +6,7 @@ import os
 
 class NekoFunctionConan(ConanFile):
     name = "neko-function"
-    version = "1.0.7"
+    version = "1.0.8"
     license = "MIT OR Apache-2.0"
     author = "moehoshio"
     url = "https://github.com/moehoshio/NekoFunction"
@@ -117,6 +117,7 @@ class NekoFunctionConan(ConanFile):
     
     def package_id(self):
         self.info.clear()
+
 
 
 
--- packages/vcpkg/neko-function/portfile.cmake
+++ packages/vcpkg/neko-function/portfile.cmake
@@ -1,8 +1,8 @@
 ﻿vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO moehoshio/NekoFunction
-    REF v1.0.7
-    SHA512 afe897309bf9c793268b34c7410fa986c5f123a1440dc6542c493c0c763642697d387df2f917c2f1dad9eb862ad03da0348e76f324289466dbcb3ff01cece81b
+    REF v1.0.8
+    SHA512 e3bdd82b44258e6a4b560b9c233bd63cdf5078cb5e0b6c1832011b3a5bd5e8eb78c403b84ea65d329c5c88e11d6bead8889908a3043f4065d1acbb58bcf5b52d
     HEAD_REF main
 )
 
@@ -43,3 +43,4 @@ endif()
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+
